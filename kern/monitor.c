20a24a4fc5a2096af75f24cb83781505795c5086
dianyuluo <1739556970@qq.com>
2024-07-09 23:17:59 +0000

    mon_backtrace 的简单实现

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "mon_backtrace", mon_backtrace }
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -57,7 +58,21 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	
+    uint32_t *ebp;
+
+    ebp = (uint32_t *)read_ebp();
+
+    cprintf("Stack backtrace:\r\n");
+/* 这个函数好就好在不会真的修改bp 寄存器上面的值,可以有效的方式物理内容发生修改 */
+    while (ebp)
+    {
+        cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\r\n", 
+                ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+
+        ebp = (uint32_t *)*ebp;
+    }
+
 	return 0;
 }
 
